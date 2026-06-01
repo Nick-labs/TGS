d85dd69a23ccca547b41ae0162db467ab716395f
Nick-labs <nikita.fomin.0404@ya.ru>
2026-06-02 00:49:38 +0300

    add hell hound

--- scripts/data/MissionData.gd
+++ scripts/data/MissionData.gd
@@ -2,8 +2,6 @@ extends Resource
 class_name MissionData
 
 @export var grid_size: Vector2i = Vector2i(10, 10)
-@export var objective_cell: Vector2i
-@export var objective_max_hp: int = 5
 @export var cp_max: int = 3
 @export var threat_growth_per_turn: int = 1
 @export var unit_spawns: Array[UnitSpawnData]
